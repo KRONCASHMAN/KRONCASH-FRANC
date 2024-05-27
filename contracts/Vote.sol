1c3e01c9dbb5c67b585b215e6d7d2bf56846e4e0
TechSolutionAAA <kogutstt2@gmail.com>
2024-05-27 13:01:55 +0200

    updated

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 contract Vote {
-    IERC721 public nftContract;
+    IERC721Enumerable public nftContract;
     ERC20 public rewardToken;
 
     struct Proposal {
@@ -13,14 +14,14 @@ contract Vote {
         uint256 votingPowerFor;
         uint256 votingPowerAgainst;
         bool executed;
-        mapping(uint256 => bool) votedForNFTs;
     }
 
+    mapping(uint256 => mapping(uint256 => bool)) public votedForNFTs;
     mapping(uint256 => uint256) public nftVotingPower;
     Proposal[] public proposals;
 
     constructor(address _nftContract, address _rewardToken) {
-        nftContract = IERC721(_nftContract);
+        nftContract = IERC721Enumerable(_nftContract);
         rewardToken = ERC20(_rewardToken);
     }
 
@@ -31,6 +32,11 @@ contract Vote {
             votingPowerAgainst: 0,
             executed: false
         }));
+        uint256 proposalId = proposals.length - 1;
+        for (uint256 i = 0; i < nftContract.balanceOf(msg.sender); i++) {
+            uint256 nftId = nftContract.tokenOfOwnerByIndex(msg.sender, i);
+            votedForNFTs[proposalId][nftId] = false;
+        }
     }
 
     function castVote(uint256 proposalId, bool inFavor) external {
@@ -38,10 +44,10 @@ contract Vote {
         require(!proposal.executed, "Proposal already executed");
 
         uint256 nftId = nftContract.tokenOfOwnerByIndex(msg.sender, 0);
-        require(!proposal.votedForNFTs[nftId], "NFT has already voted");
+        require(!votedForNFTs[proposalId][nftId], "NFT has already voted");
 
         uint256 votingPower = nftVotingPower[nftId];
-        proposal.votedForNFTs[nftId] = true;
+        votedForNFTs[proposalId][nftId] = true;
 
         if (inFavor) {
             proposal.votingPowerFor += votingPower;
@@ -58,8 +64,11 @@ contract Vote {
             proposal.executed = true;
             uint256 rewardAmount = 100 * 10 ** rewardToken.decimals();
             for (uint256 i = 0; i < proposals.length; i++) {
-                if (proposals[i].votedForNFTs[i]) {
-                    rewardToken.mint(msg.sender, rewardAmount * nftVotingPower[i]);
+                for (uint256 j = 0; j < nftContract.balanceOf(msg.sender); j++) {
+                    uint256 nftId = nftContract.tokenOfOwnerByIndex(msg.sender, j);
+                    if (votedForNFTs[i][nftId]) {
+                        rewardToken.transfer(msg.sender, rewardAmount * nftVotingPower[nftId]);
+                    }
                 }
             }
         }
